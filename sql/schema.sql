f26382339bcf4cb7321928986be97a962443556b
Mai Rachlevsky <mrachlev@umich.edu>
2021-10-11 22:16:46 -0400

    changed sql column names

--- sql/schema.sql
+++ sql/schema.sql
@@ -1,11 +1,10 @@
 PRAGMA foreign_keys = ON;
 
 CREATE TABLE users(
-  username  varchar(20) NOT NULL,
-  fullname  varchar(40) NOT NULL,
+  sender   varchar(20) NOT NULL,
+  receiver  varchar(40) NOT NULL,
   email     varchar(40) NOT NULL,
-  filename  varchar(64) NOT NULL,
-  password  varchar(256) NOT NULL,
+  phone     varchar(64) NOT NULL,
   created   datetime default current_timestamp,
-  PRIMARY KEY(username)
+  PRIMARY KEY(sender)
 );
