45a01893f261b2fb41672a2bbb444a383fdd381e
Kevin Zhu <kevin691827@gmail.com>
2021-11-08 14:31:51 -0500

    test result

--- ala/static/js/stripe.js
+++ ala/static/js/stripe.js
@@ -3,7 +3,10 @@ console.log("Sanity check!");
 
 // Get Stripe publishable key
 fetch("/config")
-.then((result) => { return result.json(); })
+.then((result) => { 
+  console.log("result back")
+  return result.json(); 
+})
 .then((data) => {
   // Initialize Stripe.js
   const stripe = Stripe(data.publicKey);
